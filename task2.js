a8abeee3cd69ed29ed0a5d3bee11cdf7cb5ef057
Shirokov Evgeniy <e.shirokov@gtcom.io>
2024-06-18 10:33:57 +0300

    add tasks

---
+++ task2.js
@@ -0,0 +1,12 @@
+// Как проверить, что строка является палиндромом?
+// Палиндром — это слово, фраза или другой тип строки, который читается одинаково слева направо и наоборот. 
+
+const strArr = ['Анна', 'Потоп']
+const numArr = [808, 880]
+
+const palindrome = arr => {
+
+}
+
+console.log(palindrome(strArr));
+console.log(palindrome(numArr));
---
+++ task3.js
@@ -0,0 +1,15 @@
+// Нужно отсортировать массив в двух направлениях по возрастанию и по убыванию
+
+const nums = [5, 23, 16, 10]
+
+const order = {
+    DESC: 'desc',
+    ASC: 'asc'
+}
+
+const sort = (arr, orderParam) => {
+
+} 
+
+console.log(sort(nums, order.ASC))
+console.log(sort(nums, order.DESC))
